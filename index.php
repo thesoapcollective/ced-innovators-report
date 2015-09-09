0d41bcd10ca8b4a16439bbee74f9c85a4f8abd30
Ian Hirschfeld <ianh7976@gmail.com>
2015-09-08 21:58:48 -0700

    Initial CED projec added.

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible'>
+  <meta content='' name='description'>
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+  <!-- <meta content='http://www.ianhirschfeld.com' property='og:url'>
+  <meta content='website' property='og:type'>
+  <meta content='' property='og:title'>
+  <meta content='.' property='og:description'>
+  <meta content='http://www.ianhirschfeld.com/open_graph.jpg' property='og:image'>
+  <link href='http://www.ianhirschfeld.com/apple-touch-icon.png' rel='apple-touch-icon'>
+  <link href='http://www.ianhirschfeld.com/favicon.png' rel='shortcut icon' type='image/x-icon'>
+  <link href='http://www.ianhirschfeld.com/favicon.png' rel='icon' type='image/x-icon'> -->
+  <title>CED</title>
+  <link href="stylesheets/normalize.css" rel="stylesheet" type="text/css" />
+  <link href="stylesheets/main.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  <div class="page-background-overlay pos-f fullsize"></div>
+  <div class="page-background-linear-gradient-overlay pos-f fullsize"></div>
+  <div class="page-background-radial-gradient-overlay pos-f"></div>
+  <div class="page-container pos-r fullsize">
+    <header class="page-header pos-f">
+      <div class="logo-container dis-ib vat pos-r">
+        <div class="logo-background pos-a"></div>
+        <div class="logo pos-a"></div>
+      </div>
+      <div class="page-title-container dis-ib vat pos-r">
+        <div class="page-title-background pos-a"></div>
+        <h1 class="page-title pos-a f-inputsans f-light f-italic fs-base">Innovators Report</h1>
+      </div>
+      <ul class="list-reset social-list dis-ib vat fs-small">
+        <li class="social-list-item dis-ib vam js-prevent-default"><a class="icons icons-twitter text-overflow dis-b a-hover-opacity" href="#">Share on Twitter</a></li>
+        <li class="social-list-item dis-ib vam js-prevent-default"><a class="icons icons-facebook text-overflow dis-b a-hover-opacity" href="#">Share on Facebook</a></li>
+        <li class="social-list-item dis-ib vam js-prevent-default"><a class="icons icons-printer text-overflow dis-b a-hover-opacity" href="#">Print Screen</a></li>
+        <li class="social-list-item dis-ib vam js-prevent-default f-adelle f-light"></li>
+      </ul>
+    </header>
+
+    <article class="page-content fullsize pos-a">
+      <section id="funding" class="content-section pos-r fullsize" data-panel="funding">
+        <header class="content-header pos-r clearfix">
+          <h2 class="content-title float-r f-inputsans f-thin f-italic">Funding</h2>
+          <ul class="list-reset filter-list pos-a">
+            <li class="filter-list-item dis-ib vat pos-r js-dropdown">
+              <span class="dropdown-title dis-ib vam f-inputsans f-thin">Year</span>
+              <span class="dropdown-slash dropdown-slash-first dis-ib vam"></span>
+              <span class="dropdown-slash dis-ib vam"></span>
+              <a class="dropdown-trigger dis-ib vam js-prevent-default" href="#">
+                <span class="dropdown-current dis-ib vam f-adelle">2015</span>
+                <span class="dropdown-arrow float-r dis-ib vam icons icons-down-arrow"></span>
+              </a>
+              <ul class="list-reset dropdown-list pos-a f-adelle">
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">2015</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">2014</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">2013</a></li>
+              </ul>
+            </li>
+            <li class="filter-list-item dis-ib vat pos-r js-dropdown">
+              <span class="dropdown-title dis-ib vam f-inputsans f-thin">Quarter</span>
+              <span class="dropdown-slash dropdown-slash-first dis-ib vam"></span>
+              <span class="dropdown-slash dis-ib vam"></span>
+              <a class="dropdown-trigger dis-ib vam js-prevent-default" href="#">
+                <span class="dropdown-current dis-ib vam f-adelle">All</span>
+                <span class="dropdown-arrow float-r dis-ib vam icons icons-down-arrow"></span>
+              </a>
+              <ul class="list-reset dropdown-list pos-a f-adelle">
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">All</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Q1</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Q2</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Q3</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Q4</a></li>
+              </ul>
+            </li>
+            <li class="filter-list-item dis-ib vat pos-r js-dropdown">
+              <span class="dropdown-title dis-ib vam f-inputsans f-thin">Sector</span>
+              <span class="dropdown-slash dropdown-slash-first dis-ib vam"></span>
+              <span class="dropdown-slash dis-ib vam"></span>
+              <a class="dropdown-trigger dis-ib vam js-prevent-default" href="#">
+                <span class="dropdown-current dis-ib vam f-adelle">All</span>
+                <span class="dropdown-arrow float-r dis-ib vam icons icons-down-arrow"></span>
+              </a>
+              <ul class="list-reset dropdown-list pos-a f-adelle">
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">All</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Tech</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Life Sci</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Adv M&amp;M</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Cleantech</a></li>
+              </ul>
+            </li>
+            <li class="filter-list-item dis-ib vat pos-r js-dropdown">
+              <span class="dropdown-title dis-ib vam f-inputsans f-thin">Type</span>
+              <span class="dropdown-slash dropdown-slash-first dis-ib vam"></span>
+              <span class="dropdown-slash dis-ib vam"></span>
+              <a class="dropdown-trigger dis-ib vam js-prevent-default" href="#">
+                <span class="dropdown-current dis-ib vam f-adelle">All</span>
+                <span class="dropdown-arrow float-r dis-ib vam icons icons-down-arrow"></span>
+              </a>
+              <ul class="list-reset dropdown-list pos-a f-adelle">
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">All</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Equity</a></li>
+                <li class="dropdown-list-item"><a class="dis-b js-prevent-default" href="#">Grants/Awards</a></li>
+              </ul>
+            </li>
+          </ul>
+        </header>
+        <div class="funding-left width-half pos-a"></div>
+        <div class="funding-right width-half pos-a"></div>
+      </section>
+
+      <section id="funders" class="content-section pos-r fullsize" data-panel="funders">
+        <h2 class="content-title float-r f-inputsans f-thin f-italic">Funders</h2>
+        <div class="clearfix"></div>
+      </section>
+
+      <section id="deals" class="content-section pos-r fullsize" data-panel="deals">
+        <h2 class="content-title float-r f-inputsans f-thin f-italic">Deals</h2>
+        <div class="clearfix"></div>
+      </section>
+
+      <section id="exits" class="content-section pos-r fullsize" data-panel="exits">
+        <h2 class="content-title float-r f-inputsans f-thin f-italic">Exits</h2>
+        <div class="clearfix"></div>
+      </section>
+    </article>
+
+    <footer class="page-footer pos-f">
+      <ul class="list-reset nav-list dis-b text-ac">
+        <li class="nav-list-item dis-ib f-inputsans is-active"><a class="js-prevent-default" href="#funding" data-panel="funding"><div class="f-xlight f-italic fs-small">Funding</div><div>&middot;</div></a></li>
+        <li class="nav-list-item dis-ib f-inputsans"><a class="js-prevent-default" href="#funders" data-panel="funders"><div class="f-xlight f-italic fs-small">Funders</div><div>&middot;</div></a></li>
+        <li class="nav-list-item dis-ib f-inputsans"><a class="js-prevent-default" href="#deals" data-panel="deals"><div class="f-xlight f-italic fs-small">Deals</div><div>&middot;</div></a></li>
+        <li class="nav-list-item dis-ib f-inputsans"><a class="js-prevent-default" href="#exits" data-panel="exits"><div class="f-xlight f-italic fs-small">Exits</div><div>&middot;</div></a></li>
+      </ul>
+    </footer>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
+  <script src="javascripts/jquery.ba-throttle-debounce.min.js" type="text/javascript"></script>
+  <script src="javascripts/jquery.panelSnap.js" type="text/javascript"></script>
+  <script src="javascripts/main.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ javascripts/main.js
@@ -0,0 +1,283 @@
+var $win = null;
+var $socialTextbox = null;
+var currentSectionIndex = 0;
+var isScrolling = false;
+
+$(document).ready(function() {
+  $win = $(window);
+  $socialTextbox = $('.social-list-item:last');
+
+  // Cancel clicks
+  $('.js-prevent-default').click(function(event) {
+    event.preventDefault();
+  });
+
+  // Social links
+  $('.social-list-item').hover(function() {
+    $socialTextbox.text($(this).text());
+    $socialTextbox.addClass('is-active');
+  }, function() {
+    $socialTextbox.removeClass('is-active');
+    $socialTextbox.text('');
+  });
+
+  // Nav and scrolling
+  $('body').panelSnap({
+    $menu: $('.nav-list'),
+    panelSelector: '.content-section',
+    menuSelector: '.nav-list-item a',
+    directionThreshold: $win.outerHeight() / 3,
+    slideSpeed: 500,
+    easing: 'swing',
+    onActivate: snappedToSection
+  });
+
+  // Filters
+  setupFilters();
+  hideCurrentFilter();
+  $('.dropdown-trigger').mouseenter(function() {
+    $(this).parent().addClass('is-active');
+  });
+  $('.js-dropdown').mouseleave(function() {
+    $(this).removeClass('is-active');
+  });
+
+  // Sections
+  setTimeout(setupFundingSection, 500);
+  // setupFundingSection();
+});
+
+var snappedToSection = function($section) {
+  var index = $section.index();
+  currentSectionIndex = index;
+  selectNavListItem();
+}
+
+var selectNavListItem = function() {
+  $('.nav-list-item.is-active').removeClass('is-active');
+  $('.nav-list-item:eq('+currentSectionIndex+')').addClass('is-active');
+}
+
+var setupFilters = function() {
+  $('.js-dropdown').each(function(i) {
+    var $this = $(this);
+    var $trigger = $('.dropdown-trigger', $this);
+    var $triggerArrow = $('.dropdown-arrow', $this);
+    var $dropdown = $('.dropdown-list', $this);
+
+    $trigger.attr('style', '');
+    $dropdown.attr('style', '');
+
+    var triggerArrowWidth = $triggerArrow.outerWidth(true);
+    var triggerWidth = $trigger.outerWidth();
+    var dropdownWidth = $dropdown.outerWidth() + triggerArrowWidth;
+
+    if (triggerWidth >= dropdownWidth) {
+      $trigger.width(triggerWidth);
+      $dropdown.width(triggerWidth);
+    } else {
+      $trigger.width(dropdownWidth);
+      $dropdown.width(dropdownWidth);
+    }
+  });
+};
+
+var hideCurrentFilter = function() {
+  $('.js-dropdown').each(function(i) {
+    var $this = $(this);
+    var current = $('.dropdown-current', $this).text();
+
+    $('.dropdown-list-item a', $this).each(function() {
+      var $that = $(this);
+      if ($that.text() === current) {
+        $that.addClass('dis-n');
+      }
+    });
+  });
+};
+
+var setupFundingSection = function() {
+  var data = [
+    {sector: 'Tech', value: 114452319},
+    {sector: 'Life Science', value: 268336928},
+    {sector: 'Advanced Manufacturing & Materials', value: 34400000},
+    {sector: 'Cleantech', value: 9747500}
+  ];
+
+  var dataColors = [
+    {name: 'blue', value: '#00dbf9'},
+    {name: 'green', value: '#4aaf77'},
+    {name: 'red', value: '#ff7662'},
+    {name: 'white', value: '#fff'}
+  ]
+
+  var colors = d3.scale.ordinal().range(dataColors);
+
+  var pieSelector = '#funding .funding-left'
+  var $pieSection = $(pieSelector)
+  var size = {
+    width: $pieSection.width(),
+    height: $pieSection.height()
+  };
+  size.radius = Math.min(size.width, size.height) / 2;
+
+  var pie = d3.layout.pie()
+    .value(function(d) { return d.value; })
+    .sort(null);
+
+  var arc = d3.svg.arc()
+    .innerRadius(0)
+    .outerRadius(size.radius - 2);
+
+  var outerArc = d3.svg.arc()
+    .innerRadius(size.radius - 2)
+    .outerRadius(size.radius);
+
+  var svg = d3.select(pieSelector)
+    .append('svg')
+      .attr('width', size.width)
+      .attr('height', size.height);
+
+  // Gradients
+  var defs = svg.append('defs');
+  dataColors.forEach(function(color) {
+    var gradient = defs.append('linearGradient')
+      .attr('id', color.name + '-gradient')
+      .attr('gradientTransform', 'rotate(45)');
+
+    gradient.append('stop')
+      .attr('offset', '0%')
+      .attr('stop-color', color.value)
+      .attr('stop-opacity', 0.1);
+
+    gradient.append('stop')
+      .attr('offset', '100%')
+      .attr('stop-color', color.value)
+      .attr('stop-opacity', 1);
+  });
+
+  var maskGadient = defs.append('radialGradient')
+    .attr('id', 'radial-mask-gradient')
+  maskGadient.append('stop')
+    .attr('offset', '0%')
+    .attr('stop-color', '#fff')
+    .attr('stop-opacity', 0);
+  maskGadient.append('stop')
+    .attr('offset', '100%')
+    .attr('stop-color', '#fff')
+    .attr('stop-opacity', 1);
+
+  // Pattern
+  var pattern = defs.append('pattern')
+    .attr('id', 'pie-pattern')
+    .attr('patternUnits', 'userSpaceOnUse')
+    .attr('width', '4')
+    .attr('height', '3')
+  pattern.append('path')
+    .attr('d', 'M0,0 L4,0 M0,1 L4,1')
+    .attr('stroke', '#000')
+    .attr('stroke-width', 1);
+
+  // Main group
+  var mask = svg.append('mask')
+    .attr('id', 'pie-mask')
+    .attr('width', size.width)
+    .attr('height', size.height);
+  mask.append('rect')
+    .attr('transform', 'translate(' + -size.radius + ',' + -size.radius + ')')
+    .attr('width', size.width)
+    .attr('height', size.height)
+    .attr('fill', 'url(#radial-mask-gradient)');
+
+  var g = svg.append('g')
+    .attr('transform', 'translate(' + size.radius + ',' + size.radius + ')')
+    .attr('class', 'pie-chart');
+
+  // Pattern slices
+  var patternArcs = g.selectAll('.pattern-arc')
+      .data(pie(data)).enter()
+      .append('g')
+        .attr('class', 'pattern-arc');
+    patternArcs.append('path')
+      .style('fill', 'url(#pie-pattern)')
+      .transition()
+        .duration(1000)
+        .attrTween('d', function(d) {
+          var i = d3.interpolate(d.startAngle, d.endAngle);
+          return function(t) {
+            d.endAngle = i(t);
+            return arc(d);
+          }
+        });
+
+  // Gradient strokes
+  var outerArcs = g.selectAll('.outer-arc')
+    .data(pie(data)).enter()
+    .append('g')
+      .attr('class', 'outer-arc');
+  outerArcs.append('path')
+    .style('fill', function(d) { return colors(d.data.sector).value; })//function(d) { return 'url(#' + colors(d.data.sector).name + '-gradient)'; })
+    .transition()
+      .duration(1000)
+      .attrTween('d', function(d) {
+        var i = d3.interpolate(d.startAngle, d.endAngle);
+        // var gradients = defs.selectAll('linearGradient');
+
+        // for (var j = 0; j < data.length; j++) {
+        //   if (data[j].sector === d.data.sector) {
+        //     var gradient = gradients[0][j];
+        //     var radians = d.endAngle - d.startAngle;
+        //     var degrees = radians * 180 / Math.PI;
+
+        //     console.log(d);
+        //     console.log(radians);
+        //     console.log(degrees);
+        //     d3.select(gradient).attr('gradientTransform', 'rotate(' + degrees + ')');
+        //   }
+        // }
+
+        return function(t) {
+          d.endAngle = i(t);
+          return outerArc(d);
+        }
+      });
+
+  // Pie slices
+  var arcs = g.selectAll('.arc')
+    .data(pie(data)).enter()
+    .append('g')
+      .attr('class', 'arc')
+      .attr('mask', 'url(#pie-mask)');
+  arcs.append('path')
+    .style('fill', function(d) { return colors(d.data.sector).value; })
+    .style('fill-opacity', '0.25')
+    .on('mouseover', function(d) {
+      d3.select(this).transition()
+        .duration(250)
+        .style('fill-opacity', '0.5');
+      d3.select(this.parentNode)
+        .attr('mask', '');
+    })
+    .on('mouseout', function(d) {
+      d3.select(this).transition()
+        .duration(250)
+        .style('fill-opacity', '0.25');
+      d3.select(this.parentNode)
+        .attr('mask', 'url(#pie-mask)');
+    })
+    .transition()
+      .duration(1000)
+      .attrTween('d', function(d) {
+        var i = d3.interpolate(d.startAngle, d.endAngle);
+        return function(t) {
+          d.endAngle = i(t);
+          return arc(d);
+        }
+      });
+
+  // arcs.append('text')
+  //   .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+  //   .attr('dy', '.35em')
+  //   .style('text-anchor', 'middle')
+  //   .text(function(d) { return d.data.sector; });
+};
